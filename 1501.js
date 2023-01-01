877878baaa14508299913b4d2e0799ec5ba29096
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-01-01 10:00:52 +0100

    added 2 ways in 1501 js

--- 1501.js
+++ 1501.js
@@ -1,6 +1,18 @@
 let fs = require('fs')
-let input = fs.readFileSync('_inputs_/1501.1').toString().trim().split('')
-let res = input.reduce((flr, c) => c == '(' ? ++flr : --flr, 0)
-let flr = 0
-let res2 = input.map(c => c == '(' ? ++flr : --flr).indexOf(-1) + 1
+let input = fs.readFileSync('_inputs_/1501.0').toString().trim().split('')
+let res = input.reduce((floor, c) => c == '(' ? ++floor : --floor, 0)
+let floor = 0
+
+// p2 golf
+// let res2 = input.map(c => c == '(' ? ++floor : --floor).indexOf(-1) + 1
+
+// p2 naive
+floor = 0
+i = -1
+while (++i < input.length) {
+	if (floor < 0)
+		break
+	floor = input[i] == '(' ? floor + 1 : floor - 1
+}
+res2 = i
 console.log(res, res2)
