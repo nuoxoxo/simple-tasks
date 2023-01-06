73efb2fc99ca45a0196afa189037aab2a9e9cacb
nuoxoxo <nuxu@e1r1p1.clusters.42paris.fr>
2023-01-06 09:30:40 +0100

    fi. 16.01 . part 2 uses brute force

--- 1601.cc
+++ 1601.cc
@@ -3,6 +3,7 @@
 #include "cstdlib"
 #include "set"
 #include "utility"
+#include "vector"
 
 using	namespace std;
 
@@ -11,12 +12,13 @@ int	main()
 	string s;
 	int r = 0, c = 0;
 	int dir = 0;
-	set<pair<int, int>> S; // p2
+	vector<vector<int>> dp = {{{0, -1}, {1, 0}, {0, 1}, {-1, 0}}}; // p1 hack
+	set<pair<int, int>> S;
 	int r2 = 0; // p2
-	int ok = 0;
+	int ok = 0; // p2
 	while (!cin.eof() && cin >> s)
 	{
-		cout << s << endl;
+		S.insert({r, c});
 		char ch = s[0];
 		int n = stoi(s.substr(1).c_str());
 		if (ch == 'R')
@@ -24,29 +26,47 @@ int	main()
 		if (ch == 'L')
 			dir -= 1;
 		dir = (dir + 4) % 4;
-		switch(dir)
+		int rr = r + n * dp[dir][0];
+		int cc = c + n * dp[dir][1];
+		// p2
+		int i, j;
+		if (ok)
 		{
-			case 0:
-				r -= n;
-				break ;
-			case 2:
-				r += n;
-				break ;
-			case 1:
-				c += n;
-				break ;
-			case 3:
-				c -= n;
-				break ;
+			r = rr, c = cc;
 		}
-		// cout << r << ' ' << c << endl;
-		
-		if (!ok && S.find({r, c}) != S.end())
+		else
 		{
-			r2 = abs(r) + abs(c);
-			ok = 1;
+			i = min(r, rr);
+			j = max(r, rr);
+			while (++i < j)
+			{
+				if (S.find({i, c}) != S.end())
+				{
+					ok = 1;
+					r2 = abs(r) + abs(i);
+					break ;
+				}
+				S.insert({i, c});
+			}
+			if (ok)
+			{
+				r = rr, c = cc;
+				continue ;
+			}
+			i = min(c, cc);
+			j = max(c, cc);
+			while (++i < j)
+			{
+				if (S.find({r, i}) != S.end())
+				{
+					r2 = abs(r) + abs(i);
+					ok = 1;
+					break ;
+				}
+				S.insert({r, i});
+			}
+			r = rr, c = cc;
 		}
-		S.insert({r, c});
 	}
 	cout << abs(r) + abs(c) << endl;
 	cout << r2 << endl;
