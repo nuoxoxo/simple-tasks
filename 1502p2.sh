5261113310b6d2dd62e23add9003706f6ab32de8
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-12-27 00:02:14 +0100

    + 2015 day 2 part 2: done

--- 1502p2.sh
+++ 1502p2.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
 
-res=0
+maxOf3 () {
+
+    local part=$4
+    local a=$1
+    local b=$2
+    local c=$3
+    local res=$a
+    if [ $part -eq 2 ]; then # part 2
+        if [ $b -gt $res ]; then res=$b; fi
+        if [ $c -gt $res ]; then res=$c; fi
+    elif [ $part -eq 1 ]; then # part 1
+        if [ $b -lt $res ]; then res=$b; fi
+        if [ $c -lt $res ]; then res=$c; fi
+    fi
+    echo $res
+}
+
+res1=0
+res2=0
 while IFS= read -r line; do
-    echo "$line"
+
+    #echo "$line"
     # echo "$line" | IFS="x" read -ra D # wrong bc. pipeline sends us to a subshell
     IFS="x" read -ra D <<< "$line"
     l="${D[0]}"
     w="${D[1]}"
     h="${D[2]}"
-    ### way 1 naive : subshell operations are slow
-    # sorted=($(for n in "${D[@]}"; do echo "$n"; done | sort -n))
-    # side=$((${sorted[0]}*${sorted[1]}))
-    ### way 2 : avoid sorting
-    min1=$l
-    min2=$w
-    if [ "$w" -lt "$l" ]; then
-        min1=$w
-        min2=$l
-    fi
-    if [ "$h" -lt "$min1" ]; then
-        min2=$min1
-        min1=$h
-    elif [ "$h" -lt "$min2" ]; then
-        min2=$h
-    fi
-    side=$(($min1*$min2))
-    temp=$((2*$l*$w + 2*$w*$h + 2*$h*$l + $side))
-    res=$(($temp+$res))
+
+    temp=$(maxOf3 $(($l*$w)) $(($w*$h)) $(($h*$l)) 1)
+    temp=$(($temp + 2*$l*$w + 2*$w*$h + 2*$h*$l))
+    res1=$((res1 + temp))
+
+    wrap=$(( $(($(($w+$l+$h)) - $(maxOf3 $l $w $h 2))) * 2 ))
+    bow=$(($w*$l*$h))
+    res2=$((res2 + wrap + bow))
+
 done
 
-echo "part 1: $res"
+echo "part 1: $res1"
+echo "part 2: $res2"
